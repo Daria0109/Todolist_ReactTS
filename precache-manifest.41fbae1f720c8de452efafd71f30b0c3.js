b1b8fb0e7bdbddb0b479969340458545ae9bd310
Daria0109 <da6ulya888@gmail.com>
2021-02-08 20:52:36 +0300

    Updates

---
+++ precache-manifest.41fbae1f720c8de452efafd71f30b0c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7004cb11b1119a373314b218966b4891",
+    "url": "/Todolist_ReactTS/index.html"
+  },
+  {
+    "revision": "39bc3defa018338f0222",
+    "url": "/Todolist_ReactTS/static/css/main.a8a956ad.chunk.css"
+  },
+  {
+    "revision": "92626fdc28e37b449c29",
+    "url": "/Todolist_ReactTS/static/js/2.e8595d13.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/Todolist_ReactTS/static/js/2.e8595d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39bc3defa018338f0222",
+    "url": "/Todolist_ReactTS/static/js/main.4832331d.chunk.js"
+  },
+  {
+    "revision": "5730afde02995adb4f9a",
+    "url": "/Todolist_ReactTS/static/js/runtime-main.e9c771df.js"
+  },
+  {
+    "revision": "3928156a651e254a3cbd86f3ac3a059e",
+    "url": "/Todolist_ReactTS/static/media/404-error.3928156a.jpg"
+  }
+]);
